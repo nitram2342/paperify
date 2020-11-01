105cff20da24f9d3d2a7d5953f542de83f2d4e22
Martin Schobert <martin@schobert.cc>
2020-11-01 19:38:14 +0100

    Adjusted URL to reference scripts with correct parameters

--- paperify.sh
+++ paperify.sh
@@ -68,7 +68,7 @@ do
     -gravity southwest -pointsize 41 -annotate +100+300 "CHUNK ${f##*-} SHA1: $chunksha\n\nFINAL SHA1: $sha" \
     -gravity northeast -pointsize 41 -annotate +100+130 "$date" \
     -gravity southeast -pointsize 41 -annotate +100+150 "$comment" \
-    -gravity southwest -pointsize 41 -annotate +100+150 "gitlab.com/alisinabh/paperify" $out
+    -gravity southwest -pointsize 41 -annotate +100+150 "gitlab.com/nitram2342/paperify" $out
 
   rm "$f"
 done
