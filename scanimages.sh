87d1588e86411688c9bb729a147a38db9ff89b65
Martin Schobert <martin@schobert.cc>
2020-11-04 15:39:50 +0100

    Check parameter or print syntax info.

--- scanimages.sh
+++ scanimages.sh
@@ -2,9 +2,18 @@
 
 dir=$1
 
+if [ -z "$dir" ]; then
+    echo Syntax: $0 "<dir>"
+    exit
+fi
+
 if [ ! -d "${dir}" ] ; then
     mkdir $dir
 fi
+
+
+echo Now scan your paper backup page by page. Afterwards, run:
+echo "digitallify.sh <outfile> ${dir}"
 
 i=0
 while true
@@ -13,9 +22,11 @@ do
     i=$((i+1))
     echo scanning $fmt_number
     
-    scanimage --resolution 300 --format=png --mode Gray --output-file ${dir}/scan.png
-    convert ${dir}/scan.png -threshold 50% -morphology open square:1 ${dir}/${fmt_number}.png
-    rm ${dir}/scan.png
+    scanimage --resolution 300 --format=png --mode Gray --output-file "${dir}/scan.png"
+    convert "${dir}/scan.png" -threshold 50% -morphology open square:1 "${dir}/${fmt_number}.png"
+    if [ -f rm "${dir}/scan.png" ] ; then	
+	rm "${dir}/scan.png"
+    fi
     echo "Hit enter to continue or Ctrl-C to stop."
     read cmd
 done
