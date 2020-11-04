545242fbd1a29ad315a4ac8a5c4e703db2c141a6
Martin Schobert <martin@schobert.cc>
2020-11-04 15:40:59 +0100

    Set error correction mode via command line switches; be able to deal with spaces in input file names; combine output QR codes to printable PDF.

--- paperify.sh
+++ paperify.sh
@@ -1,17 +1,36 @@
 #!/bin/bash -e
 
+chunk_size=2953
+error_correction_level=L
+
 for (( i=1; i<=$#; i++ ))
 do
   if [ "${!i}" = "-c" ]
-  then ((i++))
+  then
     comment=${!i};
+  elif [ "${!i}" = "-L" ]
+  then
+      :
+  elif [ "${!i}" = "-M" ]
+  then
+       chunk_size=2331
+       error_correction_level=M
+  elif [ "${!i}" = "-Q" ]
+  then
+       chunk_size=1663
+       error_correction_level=Q
+  elif [ "${!i}" = "-H" ]
+  then
+       chunk_size=1273
+       error_correction_level=H
   elif [ "${!i}" = "-h" ]
-  then ((i++))
+  then
     echo "usage: paperify.sh [OPTIONS] <FILE>"
     echo ""
     echo "options:"
-    echo -e "  -h\t\tShow this help."
-    echo -e "  -c COMMENT\tAdd comment to the generated files."
+    echo -e "  -h\t\t\tShow this help."
+    echo -e "  -L|-M|-Q|-H\t\tQR code error correction level.."
+    echo -e "  -c COMMENT\t\tAdd comment to the generated files."
     exit 0
   else
     file="$INPUT_DIR${!i}";
@@ -19,61 +38,56 @@ do
 
 done
 
-if test -f "$file"; then
-  echo "Paperifying $file"
+if test -f "${file}"; then
+  echo "Paperifying ${file}"
 else
-  echo "File not found! $file"
+  echo "File not found! ${file}"
   exit 1
 fi
 
-filename=$(echo $file | rev | cut -f 1 -d '/' | rev)
+filename=$(echo "${file}" | rev | cut -f 1 -d '/' | rev)
 prefix="$filename-"
-dir="$OUTPUT_DIR$filename-qr"
+dir="${OUTPUT_DIR}${filename}-qr"
 
-rm -Rf -- "$dir"
-mkdir -p "$dir"
+rm -Rf -- "${dir}"
+mkdir -p "${dir}"
 
-sha=$(sha1sum "$file" | cut -f 1 -d ' ')
+sha=$(sha1sum "${file}" | cut -f 1 -d ' ')
 date=$(date -u +%Y-%m-%dT%H:%M:%S+00:00)
 
-# L
-#chunk_size=2953
-# M
-#chunk_size=2331
-# Q
-chunk_size=1663
-# H
-#chunk_size=1273
+cat "${file}" | split -d -b ${chunk_size} -a3 - "${dir}/${prefix}"
 
-cat "$file" | split -d -b ${chunk_size} -a3 - "$dir/$prefix"
-
-cd "$dir"
+cd "${dir}"
 
 count=$(ls | wc -l)
 FONT=${FONT:-fixed}
 
 for f in *
 do
-  echo "processing $f"
-  chunksha=$(sha1sum "$f" | cut -f 1 -d ' ')
+  echo "Processing ${f}"
+  chunksha=$(sha1sum "${f}" | cut -f 1 -d ' ')
 
-  out="_${f/\ /_}.pdf"
-  cat "$f" | qrencode --8bit -v 40 --size=13 --margin=1 -l Q --output "$out"
+  out="_${f/\ /_}.png"
+  cat "${f}" | qrencode --8bit -v 40 --size=13 --margin=1 -l L --output "${out}"
 
-  convert -size 2863x4036 xc:white \( $out -gravity center \) -composite \
-    -font $FONT -pointsize 72 -gravity northwest -annotate +100+200 "FILE: $filename\n\nCHUNK: $f\n\nTOTAL CHUNKS: $count" \
-    -gravity southwest -pointsize 41 -annotate +100+300 "CHUNK ${f##*-} SHA1: $chunksha\n\nFINAL SHA1: $sha" \
-    -gravity northeast -pointsize 41 -annotate +100+130 "$date" \
-    -gravity southeast -pointsize 41 -annotate +100+150 "$comment" \
-    -gravity southwest -pointsize 41 -annotate +100+150 "github.com/nitram2342/paperify" $out
+  convert -size 2863x4036 xc:white \( "${out}" -gravity center \) -composite \
+    -font "${FONT}" -pointsize 72 -gravity northwest -annotate +100+200 "FILE: ${filename}\n\nCHUNK: ${f}\n\nTOTAL CHUNKS/PAGES: ${count}" \
+    -gravity southwest -pointsize 41 -annotate +100+300 "CHUNK ${f##*-} SHA1: ${chunksha}\n\nFINAL SHA1: ${sha}" \
+    -gravity northeast -pointsize 41 -annotate +100+130 "${date}" \
+    -gravity southeast -pointsize 41 -annotate +100+150 "${comment}" \
+    -gravity southwest -pointsize 41 -annotate +100+150 "github.com/nitram2342/paperify (chunksize: ${chunk_size})" "${out}"
 
-  rm "$f"
+  convert "${out}" "${out}.pdf"
+  if [ -f "${f}" ]; then
+      rm "${f}"
+  fi
 done
 
-# join pdf files
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=${filename}.pdf *.pdf
+# join pdf files, 
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite "-sOutputFile=${filename}.pdf" _*.pdf
+# pdfunite _*.pdf "${filename}.pdf"
 rm _*.pdf
 
 echo ""
-echo "QR Code generation completed"
-echo "You can now print file $dir/${filename}.pdf"
+echo "QR Code generation completed."
+echo "You can now print file: $dir/${filename}.pdf"
